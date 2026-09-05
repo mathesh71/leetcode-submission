eb829a2c1a9c14dd6e357d37b9a583d0cee08fd2
mathesh71 <matheshb71@gmail.com>
2026-09-05 19:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.85%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -10,19 +10,17 @@ class Solution:
         while list1 and list2:
             if list1.val<list2.val:
                 temp.next=list1
-                temp=temp.next
                 list1=list1.next
+                temp=temp.next
             else:
                 temp.next=list2
-                temp=temp.next
                 list2=list2.next
-        
+                temp=temp.next
+
         if list1:
             temp.next=list1
-            temp=temp.next
-            list1=list1.next
+            
         if list2:
             temp.next=list2
-            temp=temp.next
-            list2=list2.next
+            
         return dummy.next
