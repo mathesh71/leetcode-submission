024cff755228d9cdbacd002c6e13da97e820f480
mathesh71 <matheshb71@gmail.com>
2026-09-10 11:27:06 +0530

    Time: 48 ms (44.62%) | Memory: 39.3 MB (23.58%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        freq={}
+        for i,num in enumerate(nums):
+            if num in freq and i-freq[num]<=k:
+                return True
+            freq[num]=i
+        return False      
